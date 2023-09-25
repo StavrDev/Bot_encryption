bfba048e96d412b9ac9696db5b23ffe1bd0016dc
StavrDev <140498451+StavrDev@users.noreply.github.com>
2023-09-25 15:49:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils import TgBot
 from keyboards import kb_start
 
-app = TgBot('6392188988:AAGEksJrweYY1AhoL1bgNdcAcqGBmrIaDKc', kb_start)
+app = TgBot(TOKEN_BOT, kb_start)
 
 if __name__ == "__main__":
     app.start()
